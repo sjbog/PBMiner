9259cb901f51240a573beb2fffed0bf91cd7e640
Bogdan <sjbog@users.noreply.github.com>
2015-05-05 18:58:41 +0300

    name change

--- src/org/processmining/plugins/HybridMiner/loglist/LogMap.java
+++
@@ -1,8 +0,0 @@
-package org.processmining.plugins.HybridMiner.loglist;
-
-import org.deckfour.xes.model.XLog;
-
-import java.util.HashMap;
-
-public class LogMap extends HashMap< String, XLog > {}
-
